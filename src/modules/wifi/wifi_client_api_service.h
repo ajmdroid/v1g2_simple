c9399309dc2081eafa427ea9cb565879b61aeca2
ghost173-rdf <ghost173-rdf@users.noreply.github.com>
2026-02-16 17:55:42 -0500

    refactor(wifi): hide client helper routines

--- src/modules/wifi/wifi_client_api_service.h
+++ src/modules/wifi/wifi_client_api_service.h
@@ -53,28 +53,6 @@ struct Runtime {
     std::function<void()> setApMode;
 };
 
-void sendStatus(WebServer& server, const StatusPayload& payload);
-
-void sendScanInProgress(WebServer& server);
-void sendScanResults(WebServer& server,
-                     const std::vector<ScannedNetworkPayload>& networks);
-void sendScanStartFailed(WebServer& server);
-
-bool parseConnectRequest(WebServer& server,
-                         String& ssidOut,
-                         String& passwordOut,
-                         const char*& errorMessageOut);
-void sendConnectParseError(WebServer& server, const char* message);
-void sendConnectStarted(WebServer& server);
-void sendConnectStartFailed(WebServer& server);
-
-bool parseEnableRequest(WebServer& server, bool& enabledOut);
-void sendEnableParseError(WebServer& server);
-void sendEnableResult(WebServer& server, bool enabled);
-
-void sendDisconnected(WebServer& server);
-void sendForgotten(WebServer& server);
-
 void handleApiStatus(WebServer& server,
                      const Runtime& runtime,
                      const std::function<void()>& markUiActivity);
