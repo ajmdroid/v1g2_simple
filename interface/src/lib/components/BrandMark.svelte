c374fcd6e745f282375e96e1269e0cf8fc3b511c
ghost173-rdf <ghost173-rdf@users.noreply.github.com>
2026-02-19 08:57:42 -0500

    Use transparent V1 Simple logo asset in UI

--- interface/src/lib/components/BrandMark.svelte
+++ interface/src/lib/components/BrandMark.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="brand-lockup" class:compact aria-label="V1 Simple">
-	<img class="brand-logo" src="/branding/v1simple-logo.png" alt="V1 Simple" decoding="async" />
+	<img class="brand-logo" src="/branding/v1simple-logo-transparent.png" alt="V1 Simple" decoding="async" />
 </div>
