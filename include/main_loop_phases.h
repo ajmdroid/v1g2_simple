bc78dc578836c5b56701c06e5ac166fee75eb84f
ghost173-rdf <ghost173-rdf@users.noreply.github.com>
2026-02-25 09:21:43 -0500

    mod/main: extract loop phase orchestration helpers

---
+++ include/main_loop_phases.h
@@ -0,0 +1,103 @@
+/**
+ * Main loop phase helpers extracted from main.cpp.
+ *
+ * Keeps setup()/loop() orchestration readable while preserving exact behavior.
+ */
+
+#ifndef MAIN_LOOP_PHASES_H
+#define MAIN_LOOP_PHASES_H
+
+#include <Arduino.h>
+#include "modules/system/loop_connection_early_module.h"
+#include "modules/system/loop_display_module.h"
+#include "modules/system/loop_ingest_module.h"
+#include "modules/system/loop_post_display_module.h"
+#include "modules/system/loop_power_touch_module.h"
+#include "modules/system/loop_pre_ingest_module.h"
+#include "modules/system/loop_runtime_snapshot_module.h"
+#include "modules/system/loop_settings_prep_module.h"
+
+struct LoopConnectionEarlyPhaseValues {
+    bool bootSplashHoldActive = false;
+    bool initialScanningScreenShown = false;
+    bool bleConnectedNow = false;
+    bool bleBackpressure = false;
+    bool skipNonCoreThisLoop = false;
+    bool overloadThisLoop = false;
+};
+
+struct LoopIngestPhaseValues {
+    LoopSettingsPrepValues loopSettingsPrepValues;
+    bool bootReady = false;
+    bool bleBackpressure = false;
+    bool skipLateNonCoreThisLoop = false;
+    bool overloadLateThisLoop = false;
+};
+
+struct LoopDisplayPreWifiPhaseValues {
+    bool loopSignalPriorityActive = false;
+};
+
+struct LoopWifiPhaseValues {
+    LoopRuntimeSnapshotValues loopRuntimeSnapshotValues;
+    bool wifiAutoStartDone = false;
+};
+
+struct LoopFinalizePhaseValues {
+    unsigned long dispatchNowMs = 0;
+    bool bleConnectedNow = false;
+    unsigned long lastLoopUs = 0;
+};
+
+LoopConnectionEarlyPhaseValues processLoopConnectionEarlyPhase(
+    unsigned long nowMs,
+    unsigned long nowUs,
+    unsigned long lastLoopUs,
+    bool currentBootSplashHoldActive,
+    unsigned long currentBootSplashHoldUntilMs,
+    bool currentInitialScanningScreenShown);
+
+LoopIngestPhaseValues processLoopIngestPhase(
+    unsigned long nowMs,
+    bool currentBootReady,
+    unsigned long bootReadyDeadlineMs,
+    bool skipNonCoreThisLoop,
+    bool overloadThisLoop,
+    void (*runBleProcess)(),
+    void (*runBleDrain)());
+
+LoopDisplayPreWifiPhaseValues processLoopDisplayPreWifiPhase(
+    unsigned long nowMs,
+    bool bootSplashHoldActive,
+    bool overloadLateThisLoop,
+    bool enableSignalTraceLogging,
+    bool skipLateNonCoreThisLoop,
+    void (*runDisplayPipeline)(uint32_t nowMs, bool lockoutPrioritySuppressed),
+    void (*runCameraRuntime)(uint32_t nowMs,
+                             bool skipLateNonCoreThisLoop,
+                             bool overloadLateThisLoop,
+                             bool loopSignalPriorityActive));
+
+LoopWifiPhaseValues processLoopWifiPhase(
+    unsigned long nowMs,
+    unsigned long v1ConnectedAtMs,
+    bool enableWifiAtBoot,
+    bool currentWifiAutoStartDone,
+    bool skipLateNonCoreThisLoop,
+    bool bootSplashHoldActive,
+    void (*runWifiManagerProcess)());
+
+LoopFinalizePhaseValues processLoopFinalizePhase(
+    unsigned long nowMs,
+    const LoopSettingsPrepValues& loopSettingsPrepValues,
+    bool bootSplashHoldActive,
+    bool displayPreviewRunning,
+    bool bleBackpressure,
+    unsigned long scanScreenDwellMs,
+    unsigned long connectionStateProcessMaxGapMs,
+    unsigned long loopStartUs);
+
+bool shouldReturnEarlyFromLoopPowerTouchPhase(unsigned long nowMs,
+                                              unsigned long loopStartUs);
+
+#endif  // MAIN_LOOP_PHASES_H
---
+++ src/main_loop_phases.cpp
@@ -0,0 +1,210 @@
+#include "main_loop_phases.h"
+
+#include "battery_manager.h"
+#include "ble_client.h"
+#include "modules/system/loop_tail_module.h"
+#include "modules/system/periodic_maintenance_module.h"
+#include "modules/voice/voice_speed_sync_module.h"
+#include "modules/wifi/wifi_runtime_module.h"
+#include "../include/config.h"
+
+extern VoiceSpeedSyncModule voiceSpeedSyncModule;
+extern LoopConnectionEarlyModule loopConnectionEarlyModule;
+extern LoopSettingsPrepModule loopSettingsPrepModule;
+extern LoopPreIngestModule loopPreIngestModule;
+extern LoopIngestModule loopIngestModule;
+extern LoopDisplayModule loopDisplayModule;
+extern LoopPostDisplayModule loopPostDisplayModule;
+extern LoopRuntimeSnapshotModule loopRuntimeSnapshotModule;
+extern WifiRuntimeModule wifiRuntimeModule;
+extern PeriodicMaintenanceModule periodicMaintenanceModule;
+extern LoopTailModule loopTailModule;
+extern LoopPowerTouchModule loopPowerTouchModule;
+
+LoopConnectionEarlyPhaseValues processLoopConnectionEarlyPhase(
+    const unsigned long nowMs,
+    const unsigned long nowUs,
+    const unsigned long lastLoopUs,
+    const bool currentBootSplashHoldActive,
+    const unsigned long currentBootSplashHoldUntilMs,
+    const bool currentInitialScanningScreenShown) {
+    LoopConnectionEarlyContext loopConnectionEarlyCtx;
+    loopConnectionEarlyCtx.nowMs = nowMs;
+    loopConnectionEarlyCtx.nowUs = nowUs;
+    loopConnectionEarlyCtx.lastLoopUs = lastLoopUs;
+    loopConnectionEarlyCtx.bootSplashHoldActive = currentBootSplashHoldActive;
+    loopConnectionEarlyCtx.bootSplashHoldUntilMs = currentBootSplashHoldUntilMs;
+    loopConnectionEarlyCtx.initialScanningScreenShown = currentInitialScanningScreenShown;
+    const LoopConnectionEarlyResult loopConnectionEarlyResult =
+        loopConnectionEarlyModule.process(loopConnectionEarlyCtx);
+
+    LoopConnectionEarlyPhaseValues values;
+    values.bootSplashHoldActive = loopConnectionEarlyResult.bootSplashHoldActive;
+    values.initialScanningScreenShown = loopConnectionEarlyResult.initialScanningScreenShown;
+    values.bleConnectedNow = loopConnectionEarlyResult.bleConnectedNow;
+    values.bleBackpressure = loopConnectionEarlyResult.bleBackpressure;
+    values.skipNonCoreThisLoop = loopConnectionEarlyResult.skipNonCoreThisLoop;
+    values.overloadThisLoop = loopConnectionEarlyResult.overloadThisLoop;
+    return values;
+}
+
+LoopIngestPhaseValues processLoopIngestPhase(
+    const unsigned long nowMs,
+    const bool currentBootReady,
+    const unsigned long bootReadyDeadlineMs,
+    const bool skipNonCoreThisLoop,
+    const bool overloadThisLoop,
+    void (*runBleProcess)(),
+    void (*runBleDrain)()) {
+    LoopSettingsPrepContext loopSettingsPrepCtx;
+    loopSettingsPrepCtx.nowMs = nowMs;
+    const LoopSettingsPrepValues loopSettingsPrepValues =
+        loopSettingsPrepModule.process(loopSettingsPrepCtx);
+    const bool obdServiceEnabled = loopSettingsPrepValues.obdServiceEnabled;
+
+    LoopPreIngestContext loopPreIngestCtx;
+    loopPreIngestCtx.nowMs = nowMs;
+    loopPreIngestCtx.bootReady = currentBootReady;
+    loopPreIngestCtx.bootReadyDeadlineMs = bootReadyDeadlineMs;
+    loopPreIngestCtx.obdServiceEnabled = obdServiceEnabled;
+#ifdef REPLAY_MODE
+    loopPreIngestCtx.replayMode = true;
+#endif
+    const LoopPreIngestResult loopPreIngestResult = loopPreIngestModule.process(loopPreIngestCtx);
+    const bool runBleProcessThisLoop = loopPreIngestResult.runBleProcessThisLoop;
+
+    LoopIngestContext loopIngestCtx;
+    loopIngestCtx.nowMs = nowMs;
+    loopIngestCtx.bleProcessEnabled = runBleProcessThisLoop;
+    loopIngestCtx.runBleProcess = runBleProcess;
+    loopIngestCtx.runBleDrain = runBleDrain;
+    loopIngestCtx.skipNonCoreThisLoop = skipNonCoreThisLoop;
+    loopIngestCtx.overloadThisLoop = overloadThisLoop;
+    loopIngestCtx.obdServiceEnabled = obdServiceEnabled;
+    const LoopIngestResult loopIngestResult = loopIngestModule.process(loopIngestCtx);
+
+    LoopIngestPhaseValues values;
+    values.loopSettingsPrepValues = loopSettingsPrepValues;
+    values.bootReady = loopPreIngestResult.bootReady;
+    values.bleBackpressure = loopIngestResult.bleBackpressure;
+    values.skipLateNonCoreThisLoop = loopIngestResult.skipLateNonCoreThisLoop;
+    values.overloadLateThisLoop = loopIngestResult.overloadLateThisLoop;
+    return values;
+}
+
+LoopDisplayPreWifiPhaseValues processLoopDisplayPreWifiPhase(
+    const unsigned long nowMs,
+    const bool bootSplashHoldActive,
+    const bool overloadLateThisLoop,
+    const bool enableSignalTraceLogging,
+    const bool skipLateNonCoreThisLoop,
+    void (*runDisplayPipeline)(uint32_t nowMs, bool lockoutPrioritySuppressed),
+    void (*runCameraRuntime)(uint32_t nowMs,
+                             bool skipLateNonCoreThisLoop,
+                             bool overloadLateThisLoop,
+                             bool loopSignalPriorityActive)) {
+    VoiceSpeedSyncContext voiceSpeedSyncCtx;
+    voiceSpeedSyncCtx.nowMs = nowMs;
+    voiceSpeedSyncModule.process(voiceSpeedSyncCtx);
+
+    LoopDisplayContext loopDisplayCtx;
+    loopDisplayCtx.nowMs = nowMs;
+    loopDisplayCtx.bootSplashHoldActive = bootSplashHoldActive;
+    loopDisplayCtx.overloadLateThisLoop = overloadLateThisLoop;
+    loopDisplayCtx.enableSignalTraceLogging = enableSignalTraceLogging;
+    loopDisplayCtx.runDisplayPipeline = runDisplayPipeline;
+    const LoopDisplayResult loopDisplayResult = loopDisplayModule.process(loopDisplayCtx);
+    const bool loopSignalPriorityActive = loopDisplayResult.signalPriorityActive;
+
+    LoopPostDisplayContext loopPostDisplayPreWifiCtx;
+    loopPostDisplayPreWifiCtx.runAutoPushAndCamera = true;
+    loopPostDisplayPreWifiCtx.runSpeedAndDispatch = false;
+    loopPostDisplayPreWifiCtx.nowMs = nowMs;
+    loopPostDisplayPreWifiCtx.skipLateNonCoreThisLoop = skipLateNonCoreThisLoop;
+    loopPostDisplayPreWifiCtx.overloadLateThisLoop = overloadLateThisLoop;
+    loopPostDisplayPreWifiCtx.loopSignalPriorityActive = loopSignalPriorityActive;
+    loopPostDisplayPreWifiCtx.runCameraRuntime = runCameraRuntime;
+    loopPostDisplayModule.process(loopPostDisplayPreWifiCtx);
+
+    LoopDisplayPreWifiPhaseValues values;
+    values.loopSignalPriorityActive = loopSignalPriorityActive;
+    return values;
+}
+
+LoopWifiPhaseValues processLoopWifiPhase(
+    const unsigned long nowMs,
+    const unsigned long v1ConnectedAtMs,
+    const bool enableWifiAtBoot,
+    const bool currentWifiAutoStartDone,
+    const bool skipLateNonCoreThisLoop,
+    const bool bootSplashHoldActive,
+    void (*runWifiManagerProcess)()) {
+    LoopRuntimeSnapshotContext loopRuntimeSnapshotCtx;
+    const LoopRuntimeSnapshotValues loopRuntimeSnapshotValues =
+        loopRuntimeSnapshotModule.process(loopRuntimeSnapshotCtx);
+
+    WifiRuntimeContext wifiRuntimeCtx;
+    wifiRuntimeCtx.nowMs = nowMs;
+    wifiRuntimeCtx.v1ConnectedAtMs = v1ConnectedAtMs;
+    wifiRuntimeCtx.enableWifiAtBoot = enableWifiAtBoot;
+    wifiRuntimeCtx.bleConnected = loopRuntimeSnapshotValues.bleConnected;
+    wifiRuntimeCtx.canStartDma = loopRuntimeSnapshotValues.canStartDma;
+    wifiRuntimeCtx.wifiAutoStartDone = currentWifiAutoStartDone;
+    wifiRuntimeCtx.skipLateNonCoreThisLoop = skipLateNonCoreThisLoop;
+    wifiRuntimeCtx.displayPreviewRunning = loopRuntimeSnapshotValues.displayPreviewRunning;
+    wifiRuntimeCtx.bootSplashHoldActive = bootSplashHoldActive;
+    wifiRuntimeCtx.runWifiManagerProcess = runWifiManagerProcess;
+    const WifiRuntimeResult wifiRuntimeResult = wifiRuntimeModule.process(wifiRuntimeCtx);
+
+    LoopWifiPhaseValues values;
+    values.loopRuntimeSnapshotValues = loopRuntimeSnapshotValues;
+    values.wifiAutoStartDone = wifiRuntimeResult.wifiAutoStartDone;
+    return values;
+}
+
+LoopFinalizePhaseValues processLoopFinalizePhase(
+    const unsigned long nowMs,
+    const LoopSettingsPrepValues& loopSettingsPrepValues,
+    const bool bootSplashHoldActive,
+    const bool displayPreviewRunning,
+    const bool bleBackpressure,
+    const unsigned long scanScreenDwellMs,
+    const unsigned long connectionStateProcessMaxGapMs,
+    const unsigned long loopStartUs) {
+    LoopPostDisplayContext loopPostDisplayPostWifiCtx;
+    loopPostDisplayPostWifiCtx.runAutoPushAndCamera = false;
+    loopPostDisplayPostWifiCtx.runSpeedAndDispatch = true;
+    loopPostDisplayPostWifiCtx.nowMs = nowMs;
+    loopPostDisplayPostWifiCtx.configuredVoiceVolume = loopSettingsPrepValues.configuredVoiceVolume;
+    loopPostDisplayPostWifiCtx.displayUpdateIntervalMs = DISPLAY_UPDATE_MS;
+    loopPostDisplayPostWifiCtx.scanScreenDwellMs = scanScreenDwellMs;
+    loopPostDisplayPostWifiCtx.bootSplashHoldActive = bootSplashHoldActive;
+    loopPostDisplayPostWifiCtx.displayPreviewRunning = displayPreviewRunning;
+    loopPostDisplayPostWifiCtx.maxProcessGapMs = connectionStateProcessMaxGapMs;
+    const LoopPostDisplayResult loopPostDisplayResult =
+        loopPostDisplayModule.process(loopPostDisplayPostWifiCtx);
+
+    periodicMaintenanceModule.process(loopPostDisplayResult.dispatchNowMs);
+
+    LoopFinalizePhaseValues values;
+    values.dispatchNowMs = loopPostDisplayResult.dispatchNowMs;
+    values.bleConnectedNow = loopPostDisplayResult.bleConnectedNow;
+    values.lastLoopUs = loopTailModule.process(bleBackpressure, loopStartUs);
+    return values;
+}
+
+bool shouldReturnEarlyFromLoopPowerTouchPhase(const unsigned long nowMs,
+                                              const unsigned long loopStartUs) {
+#if defined(DISPLAY_WAVESHARE_349)
+    LoopPowerTouchContext loopPowerTouchCtx;
+    loopPowerTouchCtx.nowMs = nowMs;
+    loopPowerTouchCtx.loopStartUs = loopStartUs;
+    loopPowerTouchCtx.bootButtonPressed = (digitalRead(BOOT_BUTTON_GPIO) == LOW);
+    const LoopPowerTouchResult loopPowerTouchResult = loopPowerTouchModule.process(loopPowerTouchCtx);
+    return loopPowerTouchResult.shouldReturnEarly;
+#else
+    (void)nowMs;
+    (void)loopStartUs;
+    return false;
+#endif
+}
