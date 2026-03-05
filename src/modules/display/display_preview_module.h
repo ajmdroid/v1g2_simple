a7d08e5762ac8d9afbb6fe9f04d36a19bc89533a
ghost173-rdf <ghost173-rdf@users.noreply.github.com>
2026-03-05 08:21:04 -0500

    Revert "Add camera steps to display preview sequence"
    This reverts commit f60b313c2abdbf22dd17068af7e8b1b6609dcc96.

--- src/modules/display/display_preview_module.h
+++ src/modules/display/display_preview_module.h
@@ -38,31 +38,21 @@ private:
         ALERT = 0,
     };
 
-    enum class PreviewStepKind : uint8_t {
-        RADAR = 0,
-        CAMERA,
-    };
-
     struct ColorPreviewStep {
         unsigned long offsetMs;
-        PreviewStepKind kind;
         Band band;
         uint8_t bars;
         Direction dir;
         uint32_t freqMHz;
         bool muted;
-        CameraType cameraType;
-        uint16_t cameraDistanceCm;
     };
 
     static constexpr ColorPreviewStep STEPS[] = {
-        {0,    PreviewStepKind::RADAR,  BAND_X,     3, DIR_FRONT, 10525, false, CameraType::SPEED,     0},
-        {1000, PreviewStepKind::RADAR,  BAND_K,     5, DIR_SIDE,  24150, false, CameraType::SPEED,     0},
-        {2000, PreviewStepKind::RADAR,  BAND_KA,    6, DIR_REAR,  35500, false, CameraType::SPEED,     0},
-        {3000, PreviewStepKind::RADAR,  BAND_LASER, 8, static_cast<Direction>(DIR_FRONT | DIR_REAR), 0, false, CameraType::SPEED,     0},
-        {4000, PreviewStepKind::RADAR,  BAND_KA,    5, DIR_FRONT, 34700, true,  CameraType::SPEED,     0},
-        {4500, PreviewStepKind::CAMERA, BAND_NONE,  0, DIR_NONE,      0, false, CameraType::SPEED, 30480},  // ~1000 ft
-        {5000, PreviewStepKind::CAMERA, BAND_NONE,  0, DIR_NONE,      0, false, CameraType::RED_LIGHT, 15240}, // ~500 ft
+        {0,    BAND_X,    3, DIR_FRONT, 10525, false}, // X band
+        {1000, BAND_K,    5, DIR_SIDE, 24150,  false}, // K band
+        {2000, BAND_KA,   6, DIR_REAR, 35500,  false}, // Ka band
+        {3000, BAND_LASER,8, static_cast<Direction>(DIR_FRONT | DIR_REAR), 0, false}, // Laser
+        {4000, BAND_KA,   5, DIR_FRONT, 34700, true}  // Muted Ka
     };
     static constexpr int STEP_COUNT = sizeof(STEPS) / sizeof(STEPS[0]);
     static constexpr uint32_t PREVIEW_TAIL_MS = 600;  // Extra time after last step to keep frame visible
